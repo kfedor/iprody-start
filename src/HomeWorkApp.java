de43e703623aea1e5387c09da49ff729aaf6d972
Fedor Kasatkin <92401966+kfedor@users.noreply.github.com>
2024-11-13 16:45:17 +0300

    Update src/HomeWorkApp.java
    Co-authored-by: Dobrynin Maksym <dobmax@users.noreply.github.com>

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -57,7 +57,7 @@ public class HomeWorkApp {
         int value = -44;
         if (value <= 0) {
             System.out.println("Красный");
-        } else if (0 < value && value <= 100) {
+        } else if (value <= 100) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
